857c135a6e579ac6221c2cd41ba8d17fc369b913
chenglinfeng <1258781610@qq.com>
2023-08-02 23:42:46 +0800

    lab3a finished (TestSpeed3A failed: Operations completed too slowly)

--- lab2/6.5840/src/kvraft/client.go
+++ lab2/6.5840/src/kvraft/client.go
@@ -3,11 +3,17 @@ package kvraft
 import "6.5840/labrpc"
 import "crypto/rand"
 import "math/big"
+import mathrand "math/rand"
+
 
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+
+	seqId    int
+	leaderId int // 确定哪个服务器是leader，下次直接发送给该服务器
+	clientId int64
 }
 
 func nrand() int64 {
@@ -21,6 +27,8 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.clientId = nrand()
+	ck.leaderId = mathrand.Intn(len(ck.servers))
 	return ck
 }
 
@@ -37,7 +45,32 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	return ""
+	ck.seqId++
+	args := GetArgs{Key: key, ClientId: ck.clientId, SeqId: ck.seqId}
+	serverId := ck.leaderId
+	for {
+
+		reply := GetReply{}
+		//fmt.Printf("[ ++++Client[%v]++++] : send a Get,args:%+v,serverId[%v]\n", ck.clientId, args, serverId)
+		ok := ck.servers[serverId].Call("KVServer.Get", &args, &reply)
+
+		if ok {
+			if reply.Err == ErrNoKey {
+				ck.leaderId = serverId
+				return ""
+			} else if reply.Err == OK {
+				ck.leaderId = serverId
+				return reply.Value
+			} else if reply.Err == ErrWrongLeader {
+				serverId = (serverId + 1) % len(ck.servers)
+				continue
+			}
+		}
+
+		// 节点发生crash等原因
+		serverId = (serverId + 1) % len(ck.servers)
+
+	}
 }
 
 // shared by Put and Append.
@@ -50,6 +83,27 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	ck.seqId++
+	serverId := ck.leaderId
+	args := PutAppendArgs{Key: key, Value: value, Op: op, ClientId: ck.clientId, SeqId: ck.seqId}
+	for {
+
+		reply := PutAppendReply{}
+		//fmt.Printf("[ ++++Client[%v]++++] : send a %v,serverId[%v] : serverId:%+v\n", ck.clientId, op, args, serverId)
+		ok := ck.servers[serverId].Call("KVServer.PutAppend", &args, &reply)
+		if ok {
+			if reply.Err == OK {
+				ck.leaderId = serverId
+				return
+			} else if reply.Err == ErrWrongLeader {
+				serverId = (serverId + 1) % len(ck.servers)
+				continue
+			}
+		}
+
+		serverId = (serverId + 1) % len(ck.servers)
+
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
